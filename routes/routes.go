da86b362d24576f1708dd7e08fbc2116bff78f69
John Doe <mail.space.git@proton.me>
2024-01-15 09:30:43 +0300

    UploadImage Function implemented. Templates refactor. API method "uploadimage" added. Routes refactor. Controller fixes.

--- routes/routes.go
+++ routes/routes.go
@@ -11,29 +11,31 @@ type (
 	}
 )
 
-func Setup(app *fiber.App) {
+func SetupDefaultRoutes(app *fiber.App) {
 
 	//------------------------------------------------
 	// API
 	//------------------------------------------------
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Redirect("/1") // Redirect to the creator page
-	})
-
 	app.Get("/api", func(c *fiber.Ctx) error {
 		return c.Redirect("/1") // Redirect to the creator page
 	})
+
+	app.Get("/api/user/:userid", controller.GetDetails)
 
 	app.Post("/api/user/create", controller.CreateUser)
 	app.Post("/api/user/edit/:userid", controller.EditUser)
 	app.Post("/api/user/delete/:userid", controller.DeleteUser)
 
-	app.Get("/api/user/:userid", controller.GetDetails)
+	//Upload Image Handler.
+	app.Post("/api/uploadimage", controller.UploadImage)
 
 	//------------------------------------------------
 	// Main Application
 	//------------------------------------------------
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Redirect("/1") // Redirect to the creator page
+	})
 
 	app.Get("/:userid", controller.RenderUserProfile)
 }
