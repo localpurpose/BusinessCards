0072a74a7e4e093302ec8af74baec89d31164ac6
John Doe <mail.space.git@proton.me>
2024-01-16 17:11:00 +0300

    TLS/SSL Added. Some Api changes. Theme API function added. Some Fixes.

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
 	"log"
+
+	"crypto/tls"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
@@ -21,5 +24,29 @@ func main() {
 	app.Static("assets/qrs/", "./usersData")
 
 	routes.SetupDefaultRoutes(app)
-	log.Fatal(app.Listen(":80"))
+
+	m := &autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist("visitkabot.ru"),
+		Cache:      autocert.DirCache("./certs"),
+	}
+
+	cfg := &tls.Config{
+		// Get Certificate from Let's Encrypt
+		GetCertificate: m.GetCertificate,
+		// By default NextProtos contains the "h2"
+		// This has to be removed since Fasthttp does not support HTTP/2
+		// Or it will cause a flood of PRI method logs
+		// http://webconcepts.info/concepts/http-method/PRI
+		NextProtos: []string{
+			"http/1.1", "acme-tls/1",
+		},
+	}
+
+	ln, err := tls.Listen("tcp", ":443", cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Fatal(app.Listener(ln))
 }
--- models/user.go
+++ models/user.go
@@ -1,18 +1,16 @@
 package models
 
-import "time"
-
 type User struct {
-	Id           uint      `json:"id"`
-	Name         string    `json:"name"`
-	Description  string    `json:"description"`
-	Organization string    `json:"org"`
-	PhoneNumber  string    `json:"phone_number"`
-	Email        string    `json:"email"`
-	WebSite      string    `json:"webSite"`
-	TelegramName string    `json:"telegram_name"`
-	WhatsApp     string    `json:"whatsApp"`
-	QrPath       string    `json:"qrPath"`
-	CreatedAt    time.Time `json:"created_at"`
-	LastUpdate   time.Time `json:"last_update"`
+	Id           uint   `json:"id"`
+	Name         string `json:"name"`
+	Description  string `json:"description"`
+	Organization string `json:"org"`
+	PhoneNumber  string `json:"phone_number"`
+	Email        string `json:"email"`
+	WebSite      string `json:"webSite"`
+	TelegramName string `json:"telegram_name"`
+	WhatsApp     string `json:"whatsApp"`
+	QrPath       string `json:"qrPath"`
+	CreatedAt    string `json:"created_at"`
+	LastUpdate   string `json:"last_update"`
 }
