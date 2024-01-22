fa13a07d07da9b01bff3e3ef427ec60905cc46f9
amasaetre <amasaetre.software@yandex.ru>
2024-01-22 19:41:03 +0300

    script.js file's updated

--- views/brownView/assets/js/script.js
+++ views/brownView/assets/js/script.js
@@ -1,34 +1,53 @@
 function addToContacts() {
-    const nameElement = document.querySelector('.name.contact');
-    const organizationElement = document.querySelector('.org.text');
-    const phoneElement = document.querySelector('.phone.text');
-    const emailElement = document.querySelector('.email.text');
-    const websiteElement = document.querySelector('.website.text');
 
-    const name = nameElement ? nameElement.textContent.trim() : '';
-    const organization = organizationElement ? organizationElement.textContent.trim() : '';
-    const phone = phoneElement ? phoneElement.textContent.trim() : '';
-    const email = emailElement ? emailElement.textContent.trim() : '';
-    const website = websiteElement ? websiteElement.textContent.trim() : '';
+    var name = document.querySelector('.name.contact').textContent.trim();
+    var organization = document.querySelector('.org.text .org__span').textContent.trim();
+    var phone = document.querySelector('.phone.text').textContent.trim();
+    var email = document.querySelector('.email.text').textContent.trim();
+    var website = document.querySelector('.website.text').textContent.trim();
+    var telegram = document.querySelector('.telegram.text').textContent.trim();
 
-    const vCardText = `
-    BEGIN:VCARD
-    VERSION:3.0
-    N;CHARSET=UTF-8:${name}
-    ORG;CHARSET=UTF-8:"${organization}"
-    TEL:${phone}
-    EMAIL:${email}
-    URL:${website}
-    END:VCARD
-    `;
+    var photoUrl = document.querySelector('.img__container img').getAttribute('src');
 
-    const blob = new Blob([vCardText], { type: 'text/vcard;charset=utf-8' });
+    var photoBase64 = '';
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        var reader = new FileReader();
+        reader.onloadend = function () {
+            photoBase64 = reader.result.split(',')[1];
+            generateVCard();
+        };
+        reader.readAsDataURL(xhr.response);
+    };
+    xhr.open('GET', photoUrl);
+    xhr.responseType = 'blob';
+    xhr.send();
 
-    const link = document.createElement('a');
-    link.href = window.URL.createObjectURL(blob);
-    link.setAttribute('download', 'contact.vcf');
+    function generateVCard() {
 
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+        var vCardData = 'BEGIN:VCARD\n' +
+            'VERSION:3.0\n' +
+            'N:' + name + '\n' +
+            'ORG:' + organization + '\n' +
+            'TEL:' + phone + '\n' +
+            'EMAIL:' + email + '\n' +
+            'URL:' + website + '\n' +
+            'NOTE:telegram: ' + telegram + '\n' +
+            'PHOTO;TYPE=JPEG;ENCODING=BASE64:' + photoBase64 + '\n' +
+            'END:VCARD';
+
+        var blob = new Blob([vCardData], { type: 'text/vcard' });
+        var url = window.URL.createObjectURL(blob);
+
+        var a = document.createElement('a');
+        a.style.display = 'none';
+        a.href = url;
+        a.download = 'Contact.vcf';
+        document.body.appendChild(a);
+
+        a.click();
+
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+    }
 }
