d6eb045d71f7a58b20177671862df67c9ef8b4d9
John Doe <mail.space.git@proton.me>
2024-02-06 15:51:10 +0300

    Main Page realised. +style.css

--- main.go
+++ main.go
@@ -17,12 +17,15 @@ func main() {
 
 	engine := html.New("./views", ".html")
 	app := fiber.New(fiber.Config{Views: engine})
+	//Static Views Files
 	app.Static("assets/brownView/", "./views/brownView/assets")
 	app.Static("assets/blueView/", "./views/blueView/assets")
 	app.Static("assets/orangeView/", "./views/orangeView/assets")
 	app.Static("assets/pinkView/", "./views/pinkView/assets")
 	app.Static("assets/qrs/", "./usersData")
 
+	//Static Main Page files
+	app.Static("assets/main", "./views/mainpage/assets/")
 	routes.SetupDefaultRoutes(app)
 
 	m := &autocert.Manager{
